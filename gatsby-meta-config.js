ec90ea4ba6ebec2df46275b35ae55ae11bad36a0
alsth712 <alsth712@naver.com>
2023-06-01 19:57:13 +0900

    chore: 블로그 기본 세팅 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,24 +1,34 @@
 module.exports = {
-  title: `purin48`,
-  description: `purin's blog`,
+  title: `purin48's Log`,
+  description: `purin48의 블로그에 오신 걸 환영합니다`,
   author: `purin48`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  introduction: `안녕하세요 ✋ purin48의 블로그입니다! 📝`,
   siteUrl: `https://purin48.github.io/`, // Your blog site url
   social: {
-    github: `purin48/purin48.github.io/`, // Your GitHub account
-    linkedin: `in/somin-lee-4159a0271/`, // Your LinkedIn account
+    github: `purin48`, // Your GitHub account
+    linkedin: `somin-lee-4159a0271/`, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  keywords: [
+    `blog`,
+    `gatsby`,
+    `application`,
+    `react`,
+    `purin48`,
+    `Somin Lee`,
+    `FontEnd`,
+    `Developer`,
+    `purin48's Log`,
+  ],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'purin48/purin48.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'Somin Lee',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
